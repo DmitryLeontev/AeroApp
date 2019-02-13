79ce6ae7421d22e3c42c671011516aa4c2bea73c
Dmitry <dimas27.13@yandex.ru>
2019-02-13 22:27:08 +0800

    Project

---
+++ Aero/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace Aero
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            myFrame.Navigate(typeof(home), this);
+            TitleTextBlock.Text = "Главная";
+        }
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (home.IsSelected)
+            {
+                myFrame.Navigate(typeof(home), this);
+                TitleTextBlock.Text = "Главная";
+            }
+            else if (flights.IsSelected)
+            {
+                myFrame.Navigate(typeof(flights), this);
+                TitleTextBlock.Text = "Рейсы";
+            }
+            else if (orders.IsSelected)
+            {
+                myFrame.Navigate(typeof(orders), this);
+                TitleTextBlock.Text = "Заказы";
+            }
+            else if (myPortal.IsSelected)
+            {
+                myFrame.Navigate(typeof(myPortal), this);
+                TitleTextBlock.Text = "Личный кабинет";
+            }
+            else if(discountPage.IsSelected)
+            {
+                myFrame.Navigate(typeof(discountPage),this);
+                TitleTextBlock.Text = "Акции и скидки";
+            }
+            mySplitView.IsPaneOpen = false;
+        }
+        public void changeScreen(int i)
+        {
+            this.menu.SelectedIndex = i;
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            mySplitView.IsPaneOpen = !mySplitView.IsPaneOpen;
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Aero/home.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Aero
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class home : Page
+    {
+       static  MainPage myframe;
+        public home()
+        {
+            this.InitializeComponent();
+            
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter != null)
+                myframe = e.Parameter as MainPage;
+        }
+        private void GoFlight_Click(object sender, RoutedEventArgs e)
+        {
+            myframe.changeScreen(1);
+        }
+
+        private void OrdersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            myframe.changeScreen(2);
+        }
+
+        private void DiscountBtn_Click(object sender, RoutedEventArgs e)
+        {
+            myframe.changeScreen(4);
+        }
+
+        private void MyPortalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            myframe.changeScreen(3);
+        }
+    }
+}
